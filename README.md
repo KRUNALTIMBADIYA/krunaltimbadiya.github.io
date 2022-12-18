c7f18f7f9c12be5e299f5ee2e7ee9dded29b1e74
krunal timbadiya <98510266+KRUNALTIMBADIYA@users.noreply.github.com>
2022-12-18 10:43:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,67 @@
+<img align="left" height="270px">
+
+<h3> 👦: &nbsp;About Me </h3>
+
+- 😄 Pronouns:KRUNAL TIMBADIYA 
+- 🔭 I’m currently : student
+- :heart: My favorite language: python
+- 🌱 I’m currently learning: coading
+- 👯 I’m always looking to collaborate with: teacher who teach me
+- 💬 Ask me about: anything, I am happy to help
+- 🌍 I support: Latinas in Tech, AllStar Code, The Foundation to Decrease Worldsuck
+- 💜 Interests: playing, travel, dachshunds, internet culture, video games, Star Trek
+- ⚡ Fun fact: My ultimate dream is to be on interview with formal uniform. 🖖
+
+<br/>
+
+<h3> 🛠 &nbsp;Tech Stack</h3>
+
+- :space_invader:
+  ![Python](https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white)
+  ![POSTGRESQL](https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white) 
+  ![MSExcel](https://img.shields.io/badge/Microsoft_Excel-217346?style=for-the-badge&logo=microsoft-excel&logoColor=white) 
+- 🌐 &nbsp;
+  ![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+  ![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)
+  ![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+- ⚙️ &nbsp;
+  ![Git](https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white)
+  ![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)
+  ![Markdown](https://img.shields.io/badge/Markdown-000000?style=for-the-badge&logo=markdown&logoColor=white)
+- 💻 &nbsp;
+  ![Windows](https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white)
+  ![iOS](https://img.shields.io/badge/iOS-000000?style=for-the-badge&logo=ios&logoColor=white)
+
+
+<br/>
+
+<p>
+
+
+</p>
+
+
+<h3> 🤝🏻 &nbsp;Connect with Me </h3> 
+
+<p align="center">
+<br>
+<a href="https://www.linkedin.com/in/krunal-timbadiya-b95106230/"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn" /></a>&nbsp;
+<a href="https://twitter.com/Krunal_06_11?t=zIFhZvHnaV-96MtJnyYRkQ&s=09"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" alt="Twitter" /></a>&nbsp;
+<a href="https://www.youtube.com/channel/UC0Pn7d9T4JCQQNinquYpEsA/about/"><img src="https://img.shields.io/badge/youtube-12100E?style=for-the-youtube&logo=youtube&logoColor=white" alt="youtube" /></a>&nbsp;
+<a href="krunaltimbadiya2001@gmail.com"subject=Hola%20Jiji"><img src="https://img.shields.io/badge/gmail-%23D14836.svg?&style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail"/></a>&nbsp;
+
+</p>
+
+
+
+<br/> 
+<p>
+
+
+  
+<img align="center" height="270px" alt="guy" width="350" src="https://i.pinimg.com/originals/e4/26/70/e426702edf874b181aced1e2fa5c6cde.gif" /> </a>
+
+</p>
+<br/>
+
+
---
+++ portfolio.js
@@ -0,0 +1,281 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Krunal's portfolio",
+  description:
+    "Hello ** I am Krunalo Timbadiya, I am Quite Good In Web Development Into java ,c,html, I Have Achived More enough Knowledge in that. And Currently Exploring the Field of React .I am Currently Looking For an Internship in javascript, node js Or React Because I want to enhance My Knowledge and Learning Capacity.",
+  og: {
+    title: "Krunal timbadiya's portfolio",
+    type: "website",
+    url: "https://krunaltimbadiya.github.io/",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Krunal Timbadiya",
+  logo_name: "Krunal Timbadiya",
+  nickname: "WebDeveloper",
+  subTitle:
+    "I am Quite Good In Web Development Into java ,c,html I Have Achived More enough Knowledge in that. And Currently Exploring the Field of React .I am Currently Looking For an Internship in javascript,nodejs Or React Because I want to enhance My Knowledge and Learning Capacity.",
+  resumeLink:
+    "https://drive.google.com/file/d/12rt9jc0KhtZi2aRf71JaZrIY4bxI9yXj/view?usp=share_link",
+  portfolio_repository: "https://github.com/KRUNALTIMBADIYA",
+  githubProfile: "https://github.com/KRUNALTIMBADIYA",
+};
+
+const socialMediaLinks = [
+  /* Your Social Media Link */
+  
+  {
+    name: "Github",
+    link: "https://github.com/KRUNALTIMBADIYA",
+    fontAwesomeIcon: "fa-github", 
+    backgroundColor: "#181717", 
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/krunaltimbadiya06-731256233?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bf1DjMvkOQKybjQQRvJg%2FUA%3D%3D",
+    fontAwesomeIcon: "fa-linkedin-in", /
+    backgroundColor: "#0077B5", 
+  },
+  {
+    name: "YouTube",
+    link: "https://www.youtube.com/channel/UC-NNiYAfYvYo2OtPgKxuN8Q",
+    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
+    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+  },
+  {
+    name: "Gmail",
+    link: "mailto:krunaltimbadiya2001@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/Krunal_06_11?s=09",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/profile.php?id=100023297105244&mibextid=ZbWKwL",
+    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  },
+  {
+    name: "Instagram",
+    link: "https://instagram.com/_krunalpatel06?igshid=YmMyMTA2M2Y=",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+const skills = {
+  data: [
+   
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building resposive website front end using React-Redux",
+        "⚡ Developing Website using Django, ",
+        "⚡ Creating application backend in Django ",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Django",
+          fontAwesomeClassname: "simple-icons:django",
+          style: {
+            color: "#092e20",
+          },
+        },
+       
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        
+        
+        
+       
+        
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ I havebeen working on multiple cloud platforms",
+        "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
+        "⚡ I have Store My website Data in Mysql",
+        "⚡ Setting up streaming jobs from DB to Server on AWS",
+      ],
+      softwareSkills: [
+       
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+       
+        
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Mysql",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        
+      ],
+    },
+    
+  ],
+};
+
+// Education Page
+
+
+const degrees = {
+  degrees: [
+    {
+      title: "R B Institute Of Management Studies,Hiravadi",
+      subtitle: "Master of Computer Application",
+      logo_path: "RBIMS-LOGO.png",
+      alt_name: "RBIMS",
+      duration: "2022 - Present",
+      descriptions: [
+         ],
+      website_link: "http://rbi.edu.in/",
+    },
+
+    {
+      title: "C.C Sheth College Of Commerce",
+      subtitle: "Bachelor of Commerce",
+      logo_path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-t6H1UUROXNBR-YsYziFWygDVPovg8KIuPlPQnQA&usqp=CAE&s",
+      alt_name: "C.C sheth",
+      duration: "2019 - 2022",
+      descriptions: [
+        "⚡ I have studied advance methematics,account and our economy system ,etc .",
+        
+        
+      ],
+      website_link: "http://www.ccshethcommerce.com/",
+   },
+
+ {
+  title: "Gujarat Secondary and Higher Secondary Education Board, Gandhinagar",
+  subtitle: "HSC",
+  logo_path: "gseb.png",
+  alt_name: "HSC",
+  duration: "2019",
+  descriptions: [" 12th equivalent certificate to Gujarat Secondary and Higher Secondary."
+   
+  ],
+  website_link: "https://www.gseb.org/",
+},
+
+   {
+    title: "SARASWATI VIDHYAMANDIR,Uttamnagar",
+    subtitle: "SSC",
+    logo_path: "saraswati-devi-png.png",
+    alt_name: "Saraswati Vidhyamandir",
+    duration: "2017",
+    descriptions: [ "Result:67%"
+],
+    website_link: "https://entranceindia.com/city-institution-amd/saraswati-vidhya-mandir-in-khodiar-nagar-ahmedabad/",
+ },
+
+
+  ],
+
+  
+
+
+};
+    
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "https://o.remove.bg/downloads/49984be6-f8ca-4b86-9fba-b870906d96d1/8948bd17-7dc0-4b50-9f78-6fdeaaef10ca-removebg-preview.png",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with Django, React, Cloud and Opensource Development.",
+  },
+  
+  blogSection: {
+    title: "Github",
+    subtitle:
+      "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+    link: "https://github.com/KRUNALTIMBADIYA;",
+      
+    avatar_image_path: "blogs_image.svg",
+  },
+  
+  addressSection: {
+    title: "Address",
+    subtitle:
+      " Ahmedabad, Gujarat - 382350",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "https://www.google.com/maps/place/Ahmedabad,+Gujarat/@23.0204978,72.4396543,11z/data=!3m1!4b1!4m5!3m4!1s0x395e848aba5bd449:0x4fcedd11614f6516!8m2!3d23.022505!4d72.5713621",
+  },
+  phoneSection: {
+    title: "Phone Number",
+    subtitle: "+91 6352145877",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+
+  degrees,
+  certifications,
+  
+  contactPageData,
+};
