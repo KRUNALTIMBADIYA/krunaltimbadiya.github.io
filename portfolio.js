5b42838205bac89dae0623bf52753390ce8f8f19
krunal timbadiya <98510266+KRUNALTIMBADIYA@users.noreply.github.com>
2022-12-19 10:06:51 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -47,7 +47,7 @@ const socialMediaLinks = [
   },
   {
     name: "YouTube",
-    link: "https://www.youtube.com/channel/UC-NNiYAfYvYo2OtPgKxuN8Q",
+    link: "https://https://youtube.com/@krunaltimbadiya7862",
     fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
     backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
   },
